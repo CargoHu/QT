3b137ad108e3669a0f9b6d79a19b9d4ad356d299
CargoHu <hu.jia.xuan@163.com>
2017-06-10 16:17:26 +0800

    qt test
    moc test

---
+++ Test2/hjxclass.cpp
@@ -0,0 +1,19 @@
+#include "hjxclass.h"
+#include <iostream>
+using namespace std;
+HjxClass::HjxClass()
+{
+
+}
+void HjxClass::slots1(int i,double j){
+    cout<<i<<j;
+}
+double HjxClass::slots2(double j){
+    cout<<j;
+    return j;
+}
+//void HjxClass::signalFunc(double i){cout<<i;}
+//int HjxClass::signalFunc2(char j, int k){
+//    cout<<j<<k;
+//}
+
---
+++ Test2/hjxclass.h
@@ -0,0 +1,36 @@
+#ifndef HJXCLASS_H
+#define HJXCLASS_H
+
+#include <QObject>
+
+class HjxClass :public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(bool enabled READ isEnabled WRITE setEnabled)
+    Q_PROPERTY(QString title READ getTitle WRITE setTitle USER true)
+public:
+    HjxClass();
+
+
+    bool  isEnabled(){
+        return true;
+    }
+    void  setTitle(QString t){title = t;}
+    QString  getTitle(){return "null";}
+    void  setEnabled(bool e) { enabled = !e; }
+
+    bool isEnabled() const { return enabled; }
+    bool enabled;
+    QString title;
+public slots:
+    void slots1(int i,double j);
+    double slots2(double j);
+signals:
+    void signalFunc(double);
+    int signalFunc2(char, int);
+    int signalFunc3(char, int);
+    int sf4(char, int);
+
+};
+
+#endif // HJXCLASS_H
